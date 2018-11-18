725f5b548f412102b8ddb43b7331c5878159250f
MrMacon <2631608607@qq.com>
2018-11-18 11:17:41 +0800

    home

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -3,6 +3,11 @@ import { View, Image, Text, Button } from 'react-native'
 
 
 export default class DashBoard extends Component {
+    static defaultProps = {
+        number: 12314111
+    }
+
+    
 
     render() {
         return (
