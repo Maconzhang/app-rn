c8cea35b5325eebd2841d79429afaf6836bc4b2b
HF-20180713FWTX\Administrator <944626347@qq.com>
2018-11-30 17:35:56 +0800

    11 30 crash

--- android/app/src/main/java/com/fapp/MainApplication.java
+++ android/app/src/main/java/com/fapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.fapp;
 import android.app.Application;
 // import com.lwansbrough.RCTCamera.*;
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 // import com.eguma.barcodescanner.BarcodeScannerPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -28,8 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             // new BarcodeScannerPackage(),
-           new RCTCameraPackage(),
             new MapsPackage(),
             new RNCameraPackage(),
             new AMap3DPackage(),
